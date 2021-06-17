f3b07e2d7813a7aed2094deeefa89f53c0d1bf9a
Magnus Ögren <magnus.ogren@gmail.com>
2021-06-17 09:53:57 +0200

    omdomen

--- routes/omdomen.js
+++ routes/omdomen.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const Omdome = require('../models/omdome');
 const Golfbana = require('../models/golfbana');
 const catchAsync = require('../utils/catchAsync');
