bb56a415a11291f5a146b1c56be111ceefbd0978
Magnus Ögren <magnus.ogren@gmail.com>
2021-06-19 20:50:40 +0200

    jobbar med ny, omdömen. routes

--- models/golfrunda.js
+++ models/golfrunda.js
@@ -1,44 +1,48 @@
+const { number } = require('joi');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BilderSchema = new Schema({
-	url: String,
-	filename: String
+  url: String,
+  filename: String,
 });
 
 const GolfrundaSchema = new Schema({
-	datum: Date,
-	bana: String,
-	antalHal: Number,
-	banansPar: Number,
-	tee: {
-		type: {
-			type: String,
-			enum: [ 'Gul', 'Röd' ]
-		}
-	},
-	langd: String,
-	bruttoScore: Number,
-	nettoScore: Number,
-	poang: Number,
-	vader: String,
-	vindstyrka: Number,
-	temperatur: Number,
-	noteringar: String,
-	banansSkick: Number,
-	sammanfattningBetyg: Number,
-	// bilder: [BilderSchema],
-	url: String,
-	// spelare: String,
-	medspelare: String,
-	spelare: {
-		type: Schema.Types.ObjectId,
-		ref: 'Anvandare'
-	}
-	// medspelare: {
-	//   type: Schema.Types.ObjectId,
-	//   ref: 'Anvandare',
-	// },
+  datum: Date,
+  bana: String,
+  antalHal: Number,
+  banansPar: Number,
+  tee: {
+    type: {
+      type: String,
+      enum: ['Gul', 'Röd'],
+    },
+  },
+  langd: String,
+  bruttoScore: Number,
+  nettoScore: Number,
+  poang: Number,
+  vader: String,
+  vindstyrka: Number,
+  temperatur: Number,
+  noteringar: String,
+  banansSkick: Number,
+  sammanfattningBetyg: Number,
+  // bilder: [BilderSchema],
+  url: String,
+  // spelare: String,
+  medspelare: String,
+  spelare: {
+    type: Schema.Types.ObjectId,
+    ref: 'Anvandare',
+  },
+  betyg: Number,
+  text: String,
+
+  // medspelare: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: 'Anvandare',
+  // },
 });
 
 module.exports = mongoose.model('Golfrunda', GolfrundaSchema);
--- routes/golfdagbok.js
+++ routes/golfdagbok.js
@@ -2,82 +2,98 @@ const express = require('express');
 const router = express.Router();
 const Golfrunda = require('../models/golfrunda');
 const Golfbana = require('../models/golfbana');
+const Omdome = require('../models/omdome');
 // const { updateCampground } = require('../../YelpCamp/magnusVersion/controllers/campgrounds');
 const { valideraGolfrunda } = require('../middleware');
 const catchAsync = require('../utils/catchAsync');
+const golfbana = require('../models/golfbana');
 
 router.get(
-	'/',
-	catchAsync(async (req, res) => {
-		const golfbanor = await Golfbana.find({});
-		const golfrundor = await Golfrunda.find({});
-		// console.log(golfdagbok);
-		res.render('golfdagbok', { golfbanor, golfrundor });
-	})
+  '/',
+  catchAsync(async (req, res) => {
+    const golfbanor = await Golfbana.find({});
+    const golfrundor = await Golfrunda.find({});
+    // console.log(golfdagbok);
+    res.render('golfdagbok', { golfbanor, golfrundor });
+  })
 );
 
 router.get(
-	'/ny',
-	catchAsync(async (req, res) => {
-		const golfbanor = await Golfbana.find({});
-		res.render('golfdagbok/ny', { golfbanor });
-	})
+  '/ny',
+  catchAsync(async (req, res) => {
+    const golfbanor = await Golfbana.find({});
+    res.render('golfdagbok/ny', { golfbanor });
+  })
 );
 
 router.post(
-	'/',
-	valideraGolfrunda,
-	catchAsync(async (req, res, next) => {
-		const golfrunda = new Golfrunda(req.body.golfrunda);
-		const url = req.body.url;
-		golfrunda.bilder = { url };
-		golfrunda.spelare = req.user._id;
-		// console.log(golfrunda);
-		await golfrunda.save();
-		res.redirect(`golfdagbok/${golfrunda._id}`);
-	})
+  '/',
+  valideraGolfrunda,
+  catchAsync(async (req, res, next) => {
+    const golfrunda = new Golfrunda(req.body.golfrunda);
+    const { betyg, text } = req.body;
+    const url = req.body.url;
+    golfrunda.bilder = { url };
+    golfrunda.spelare = req.user._id;
+    if (betyg !== 1 && text !== '') {
+      const omdome = new Omdome();
+      omdome.text = text;
+      omdome.betyg = betyg;
+      const golfbana = await Golfbana.find({ name: `${golfrunda.bana}` });
+      golfbana.omdomen.push(omdome);
+    }
+    await golfrunda.save();
+    res.redirect(`golfdagbok/${golfrunda._id}`);
+  })
+);
+
+// const { id } = req.params;
+//     const golfbana = await Golfbana.findById(id);
+//     const omdome = new Omdome(req.body.omdome);
+//     golfbana.omdomen.push(omdome);
+//     await omdome.save();
+//     await golfbana.save();
+
+router.get(
+  '/:id',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const golfrunda = await Golfrunda.findById(id).populate('spelare');
+    console.log(req.session, req.user);
+    res.render('golfdagbok/visa', { golfrunda });
+  })
 );
 
 router.get(
-	'/:id',
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		const golfrunda = await Golfrunda.findById(id).populate('spelare');
-		console.log(req.session, req.user);
-		res.render('golfdagbok/visa', { golfrunda });
-	})
-);
-
-router.get(
-	'/:id/andra',
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		const golfbanor = await Golfbana.find({});
-		const golfrunda = await Golfrunda.findById(id);
-		// console.log(golfrunda);
-		res.render('golfdagbok/andra', { golfrunda, golfbanor });
-	})
+  '/:id/andra',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const golfbanor = await Golfbana.find({});
+    const golfrunda = await Golfrunda.findById(id);
+    // console.log(golfrunda);
+    res.render('golfdagbok/andra', { golfrunda, golfbanor });
+  })
 );
 
 router.put(
-	'/:id',
-	valideraGolfrunda,
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		const updateradgolfrunda = await Golfrunda.findByIdAndUpdate(id, { ...req.body.golfrunda });
-		console.log(updateradgolfrunda);
-		await updateradgolfrunda.save();
-		res.redirect(`/golfdagbok/${updateradgolfrunda._id}`);
-	})
+  '/:id',
+  valideraGolfrunda,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const updateradgolfrunda = await Golfrunda.findByIdAndUpdate(id, { ...req.body.golfrunda });
+    console.log(updateradgolfrunda);
+    await updateradgolfrunda.save();
+    res.redirect(`/golfdagbok/${updateradgolfrunda._id}`);
+  })
 );
 
 router.delete(
-	'/:id',
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		await Golfrunda.findByIdAndDelete(id);
-		res.redirect('/golfdagbok');
-	})
+  '/:id',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Golfrunda.findByIdAndDelete(id);
+    res.redirect('/golfdagbok');
+  })
 );
 
 module.exports = router;
